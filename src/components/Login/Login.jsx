8bf1584e5f88249d0fa2203231aefbddbf961a6b
Mashal <zyadomar112@gmail.com>
2024-10-14 23:09:33 +0300

    update error

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -16,17 +16,6 @@ const Login = () => {
   };
   return (
     <>
-      <div className="errorPage">
-        <div className="errorPage_container">
-          <div className="error_404">
-            <h1>404</h1>
-            <img src={error} alt="error 404" />
-          </div>
-          <p>هذه المنصة لا تعمل الا علي شاشة الهاتف</p>
-          <p>يرجي استخدام الهاتف لعدم التعرض الي ضياع الحساب</p>
-          <span>Dr: Python</span>
-        </div>
-      </div>
       <div className="view_content">
         <section className="login">
           <div className="login_container">
